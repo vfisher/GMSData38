cb5028a37f6a67edf379742a90bdfb766f71bb58
vfisher <vfisher@users.noreply.github.com>
2025-01-20 15:18:19 +0200

    t_SaleGetRetContractID

--- DB/Tables/dbo.r_POSPays.sql
+++ DB/Tables/dbo.r_POSPays.sql
@@ -16,7 +16,8 @@ CREATE TABLE [dbo].[r_POSPays]
 [UsePosCollection] [bit] NOT NULL DEFAULT ((0)),
 [SettleBeforeRefund] [bit] NOT NULL DEFAULT ((1)),
 [POSMerchantId] [int] NOT NULL DEFAULT ((1)),
-[UsePosCompareCR] [bit] NOT NULL DEFAULT ((0))
+[UsePosCompareCR] [bit] NOT NULL DEFAULT ((0)),
+[UseFacePay] [bit] NOT NULL DEFAULT ((0))
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[r_POSPays] ADD CONSTRAINT [pk_r_POSPays] PRIMARY KEY CLUSTERED ([POSPayID]) ON [PRIMARY]
