589744357482a4342e9a1b3f57d8697138f55c28
Timakova <shevchenko@gms.com.ua>
2025-01-28 13:50:53 +0200

    AVR-5569: Правки

--- DB/Stored Procedures/dbo.t_SaleGetProdImage.sql
+++ DB/Stored Procedures/dbo.t_SaleGetProdImage.sql
@@ -7,7 +7,7 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	DECLARE @ProdImage varbinary(max)
-	SET @ProdImage = (SELECT Picture FROM r_ProdImages WHERE ProdID = @ProdID)
+	SET @ProdImage = (SELECT TOP 1 Picture FROM r_ProdImages WHERE ProdID = @ProdID AND IsMain = 1)
 	
 	SELECT TOP 1 ProdImage = @ProdImage, ProdImageURL = ''   
 END
