997f192183e0a98cf63fc9e3a256c68856f3a4a4
Timakova <shevchenko@gms.com.ua>
2025-08-14 01:36:51 +0300

    DEC-3: Fix

--- SQL/Programmability/Procedures/dbo.t_GetCRChequeByDocCode.sql
+++ SQL/Programmability/Procedures/dbo.t_GetCRChequeByDocCode.sql
@@ -10,11 +10,12 @@ BEGIN
   DECLARE @DocCode int, @ChID bigint
   DECLARE @UseProdNotes bit, @GroupProds bit, @TaxPayer bit
   DECLARE @OurID int, @SaleDocDate smalldatetime
-  DECLARE @CashType int, @UseHardwareDisc bit, @SaleRoundDiscCode int
+  DECLARE @CashType int, @UseHardwareDisc bit, @SaleRoundDiscCode int, @RoundInCheque bit
 
   SET @UseHardwareDisc = 0
   SET @CashType = 0
   SET @SaleRoundDiscCode = ISNULL(dbo.zf_Var('t_SaleRoundDiscCode'),-1)
+  SET @RoundInCheque = 0
 
   SET @DocCode = JSON_VALUE(@ParamsIn, '$.DocCode')
   SET @ChID = JSON_VALUE(@ParamsIn, '$.ChID')
@@ -23,8 +24,9 @@ BEGIN
     BEGIN
 	     SELECT 
 		     @UseProdNotes = c.UseProdNotes, 
-		     @GroupProds = c.GroupProds, 
-		     @TaxPayer = dbo.zf_GetTaxPayerByDate(m.OurID, m.DocTime) 
+		     @GroupProds = c.GroupProds,
+			 @CashType = c.CashType,
+		     @TaxPayer = dbo.zf_GetTaxPayerByDate(m.OurID, m.DocTime)
 	     FROM t_SaleTemp m WITH(NOLOCK), r_CRs c WITH(NOLOCK), r_Ours o WITH(NOLOCK)  
 	     WHERE m.ChID = @ChID AND c.CRID = m.CRID AND m.OurID = o.OurID 
 
@@ -52,25 +54,22 @@ BEGIN
 		     @GroupProds = c.GroupProds, 
 		     @TaxPayer = dbo.zf_GetTaxPayerByDate(m.OurID, m.DocTime),
 			 @CashType = c.CashType,
-             @UseHardwareDisc = CASE WHEN pw.DiscountMode = 1 THEN 1 ELSE 0 END
+             @UseHardwareDisc = CASE WHEN pw.DiscountMode IN (1,3) THEN 1 ELSE 0 END,
+			 @RoundInCheque = pw.RoundInCheque
 	     FROM t_Sale m WITH(NOLOCK), r_CRs c WITH(NOLOCK), r_Ours o WITH(NOLOCK), r_WPs rw WITH(NOLOCK), r_WPRoles pw WITH(NOLOCK)   
-	     WHERE m.ChID = @ChID AND c.CRID = m.CRID AND m.OurID = o.OurID AND m.WPID = rw.WPID AND rw.WPRoleID = pw.WPRoleID  
-	  print @UseHardwareDisc
+	     WHERE m.ChID = @ChID AND c.CRID = m.CRID AND m.OurID = o.OurID AND m.WPID = rw.WPID AND rw.WPRoleID = pw.WPRoleID 
 	     SET @ParamsOut = (
 	     SELECT 
 		     Doc = ISNULL((SELECT m.*, m.SaleRndSum AS RndSum FROM t_Sale m WITH(NOLOCK) WHERE m.CHID = @ChID FOR JSON PATH), '{}'),
              DocD = ISNULL((SELECT m.ChID, m.SrcPosID, m.ProdID, m.PPID, m.UM, m.Qty, m.PriceCC_nt, m.SumCC_nt, m.Tax, m.TaxSum, m.PriceCC_wt, m.SumCC_wt, m.BarCode, m.SecID, m.PurPriceCC_nt, m.PurTax, m.PurPriceCC_wt, m.PLID, m.Discount, m.EmpID, m.CreateTime, m.ModifyTime, m.TaxTypeID, 
-			   CASE WHEN @UseHardwareDisc = 1 THEN CASE WHEN @CashType = 39 THEN ISNULL(m.PurPriceCC_wt,0) ELSE m.RealPrice END ELSE m.RealPrice END RealPrice,
-			   CASE WHEN @UseHardwareDisc = 1 THEN CASE WHEN @CashType = 39 THEN ROUND((ISNULL(m.PurPriceCC_wt,0) * m.Qty),2) ELSE m.RealSum END ELSE m.RealSum END RealSum,
+			   CASE WHEN @UseHardwareDisc = 1 THEN CASE WHEN @CashType = 39 AND @RoundInCheque = 1 THEN ISNULL(m.PurPriceCC_wt,0) ELSE m.RealPrice END ELSE m.RealPrice END RealPrice,
+			   CASE WHEN @UseHardwareDisc = 1 THEN CASE WHEN @CashType = 39 AND @RoundInCheque = 1 THEN ROUND((ISNULL(m.PurPriceCC_wt,0) * m.Qty),2) ELSE m.RealSum END ELSE m.RealSum END RealSum,
 			   m.MarkCode, m.LevyMark,
 			   p.ProdName, p.CstProdCode,
-			   CASE WHEN @UseHardwareDisc = 1 THEN CASE WHEN @CashType = 39 THEN ISNULL(l.SumBonus,0) ELSE m.Discount END ELSE 0 END DiscountSum,
-			   ISNULL(lrnd.SumBonus,0) RndSum,
+			   CASE WHEN @UseHardwareDisc = 1 THEN CASE WHEN @CashType = 39 AND @RoundInCheque = 1 THEN ROUND((ISNULL(m.PurPriceCC_wt,0) * m.Qty),2) - m.RealSum ELSE m.Discount END ELSE 0 END DiscountSum,
 		       CASE WHEN @TaxPayer = 1 THEN m.TaxTypeID ELSE 1 END TaxTypeIDWithCheckTax
 		 FROM t_SaleD m WITH(NOLOCK)
 		 INNER JOIN r_Prods p WITH(NOLOCK) ON m.ProdID = p.ProdID
-		 LEFT JOIN (SELECT SrcPosID, SUM(ISNULL(SumBonus,0)) AS SumBonus FROM z_LogDiscExpP WITH(NOLOCK) WHERE DocCode = 11035 AND ChID = @ChID AND DiscCode <> @SaleRoundDiscCode GROUP BY SrcPosID) l ON m.SrcPosID = l.SrcPosID  
-		 LEFT JOIN (SELECT SrcPosID, SUM(ISNULL(SumBonus,0)) AS SumBonus FROM z_LogDiscExpP WITH(NOLOCK) WHERE DocCode = 11035 AND ChID = @ChID AND DiscCode = @SaleRoundDiscCode GROUP BY SrcPosID) lrnd ON m.SrcPosID = lrnd.SrcPosID
 		 WHERE m.CHID = @ChID FOR JSON PATH),'{}'),
 
             /* Если форма оплаты: Картой с выдачей наличных, то нам нужно получить запись только по 2 форме оплаты */
