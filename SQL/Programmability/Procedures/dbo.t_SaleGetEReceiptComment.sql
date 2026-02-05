e4812dfdd0706cac4d33e83df053953d3d11a77b
timakova <shevchenko@gms.com.ua>
2026-02-05 13:30:54 +0200

    AVR-7214: Формування коментаря програми єЧек

--- SQL/Programmability/Procedures/dbo.t_SaleGetEReceiptComment.sql
+++ SQL/Programmability/Procedures/dbo.t_SaleGetEReceiptComment.sql
@@ -1,48 +1,72 @@
 ﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
 CREATE PROCEDURE [dbo].[t_SaleGetEReceiptComment](@ParamsIn nvarchar(max))
+/* Повертає коментар до чека для підтримки програми «єЧек» */
 AS
 BEGIN
-  DECLARE @CRID int, @DocCode int, @ChID bigint 
-  DECLARE @TransactionInfo nvarchar(max) 
-  DECLARE @BID nvarchar(max), @RID bigint, @TIN varchar(10), @BNS varchar(max), @RWR varchar(64), @BTX varchar(255) 
-  /* SET @ParamsIn = '{"DocCode":11035, "ChID":100000143, "CRID":2}' */
+    DECLARE 
+        @CRID int,
+        @DocCode int,
+        @ChID bigint,
+        @DocID bigint,
+        @OurID int,
+        @ChequeComment nvarchar(500),
+        @TIN varchar(10),
+        @mcr nchar(2) = CHAR(13) + CHAR(10),
+        @CRComment nvarchar(1024)
 
-  SET @CRID = JSON_VALUE(@ParamsIn, '$.CRID')
-  SET @DocCode = JSON_VALUE(@ParamsIn, '$.DocCode')
-  SET @ChID = JSON_VALUE(@ParamsIn, '$.ChID')
-  SET @TransactionInfo = '{}'
+  /* SET @ParamsIn = '{"OurID":7,"ChequeComment":"ЧЕК:100000009 ПРРО GMS ТЕСТ (касса 1)","DocCode":1011,"DocID":100000009,"ChID":100000117,"CRID":25}' */
+  /* 
+    <COMMENT>ERECEIPT
+    BID=123e4567-e89b-12d3-a456-426614174000
+    RID=bar:20240101-123456789
+    TIN=1234567890
+    BNS=acpenfb
+    RWR=2312 балів
+    BTX=Замовлення №12345, дякуємо за покупку!</COMMENT>
+  */
 
-  IF @DocCode = 11035 
-    SELECT @TransactionInfo = TransactionInfo FROM t_SalePays WHERE ChID = @ChID
-  ELSE IF @DocCode = 11004
-    SELECT @TransactionInfo = TransactionInfo FROM t_CRRetPays WHERE ChID = @ChID
-select @TransactionInfo
-/* 
-<COMMENT>ERECEIPT
-BID=123e4567-e89b-12d3-a456-426614174000
-RID=bar:20240101-123456789
-TIN=1234567890
-BNS=acpenfb
-RWR=2312 балів
-BTX=Замовлення №12345, дякуємо за покупку!</COMMENT>
-*/
+    SELECT
+        @CRID          = JSON_VALUE(@ParamsIn, '$.CRID'),
+        @DocCode       = JSON_VALUE(@ParamsIn, '$.DocCode'),
+        @ChID          = JSON_VALUE(@ParamsIn, '$.ChID'),
+        @OurID         = JSON_VALUE(@ParamsIn, '$.OurID'),
+        @DocID         = JSON_VALUE(@ParamsIn, '$.DocID'),
+        @ChequeComment = JSON_VALUE(@ParamsIn, '$.ChequeComment')
 
-  SET @BID = JSON_VALUE(@ParamsIn, '$.adv.natr')
-  SET @RID = JSON_VALUE(@ParamsIn, '$.adv.rid')
-  SET @TIN = ''
-  SET @BNS = ''
-  SET @RWR = ''
-  SET @BTX = ''
-print 1
-SELECT
-    'ERECEIPT' + CHAR(10) +
-    'BID=' + @BID + CHAR(10) +
-    'RID=' + CAST(@RID AS VARCHAR(max)) + CHAR(10) +
-    'TIN=' + @TIN + CHAR(10) +
-    'BNS=' + @BNS + CHAR(10) +
-    'RWR=' + @RWR + CHAR(10) +
-    'BTX=' + @BTX 
-    AS Comment
+    SELECT @TIN = Code FROM r_Ours WITH (NOLOCK) WHERE OurID = @OurID
+
+    SELECT
+        @CRComment = STUFF((
+            SELECT @mcr +
+                   'ERECEIPT' + @mcr +
+                   'BID=' + ISNULL(JSON_VALUE(JSON_VALUE(p.TransactionInfo,'$.POSPayAdv'),'$.natr'),'') + @mcr +
+                   'RID=' + ISNULL(JSON_VALUE(JSON_VALUE(p.TransactionInfo,'$.POSPayAdv'),'$.rid'),'')  + @mcr +
+                   'TIN=' + @TIN + @mcr +
+                   'BTX=Замовлення №' + CAST(@DocID AS varchar(20)) + ', дякуємо за покупку!'
+            FROM (
+                SELECT TransactionInfo
+                FROM t_SaleTempPays
+                WHERE @DocCode = 1011 AND ChID = @ChID
+
+                UNION ALL
+                SELECT TransactionInfo
+                FROM t_SalePays
+                WHERE @DocCode = 11035 AND ChID = @ChID
+
+                UNION ALL
+                SELECT TransactionInfo
+                FROM t_CRRetPays
+                WHERE @DocCode = 11004 AND ChID = @ChID
+            ) p
+            WHERE
+                ISJSON(JSON_VALUE(p.TransactionInfo,'$.POSPayAdv')) = 1
+            FOR XML PATH(''), TYPE
+        ).value('.', 'nvarchar(max)'), 1, LEN(@mcr), '')
+
+    SELECT
+        CASE WHEN @ChequeComment <> '' 
+          THEN @ChequeComment + @mcr 
+        ELSE '' END + ISNULL(@CRComment,'') AS CRComment
 END
 GO
