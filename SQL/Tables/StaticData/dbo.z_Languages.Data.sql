fb32fa22672d1241ecd21f7198d4a0200ac7a7c4
timakova <shevchenko@gms.com.ua>
2026-06-05 17:33:09 +0300

    AVR-7466: Монітор перекладу

---
+++ SQL/Tables/StaticData/dbo.z_Languages.Data.sql
@@ -0,0 +1,4 @@
+﻿INSERT dbo.z_Languages(LanguageID, LanguageCode, LanguageName) VALUES (1, 'en', N'English');
+INSERT dbo.z_Languages(LanguageID, LanguageCode, LanguageName) VALUES (2, 'uk', N'Ukrainian');
+INSERT dbo.z_Languages(LanguageID, LanguageCode, LanguageName) VALUES (3, 'ru', N'Russian');
+INSERT dbo.z_Languages(LanguageID, LanguageCode, LanguageName) VALUES (4, 'ro', N'Romanian');
